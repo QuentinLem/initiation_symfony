03c52076e530f2dbac3fe44411906ae448f80f37
Quentin L. <quentin.lemonnier.info@gmail.com>
2024-05-27 19:07:11 +0200

    templating bootstrap/twig + fragments + url()/path()

--- src/Controller/MyFirstController.php
+++ src/Controller/MyFirstController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 class MyFirstController extends AbstractController
 {
     #[Route('/my/first', name: 'app_my_first')]
@@ -16,6 +17,20 @@ class MyFirstController extends AbstractController
             'path' => '',
             'nom' => 'Quentin',
             'age' => '18',
+        ]);
+    }
+
+    #[Route('/template', name: 'app_template')]
+    public function template(): Response
+    {
+        return $this->render('templateBootstrap.html.twig');
+    }
+
+    public function sayHello($firstname, $name): Response
+    {
+        return $this->render('my_first\hello.html.twig', [
+            'firstname' => $firstname,
+            'name' => $name,
         ]);
     }
 }
