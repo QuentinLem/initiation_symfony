04c5be0ceb31e27fd379cee5c7c70908b2e5fd1b
Quentin L. <quentin.lemonnier.info@gmail.com>
2024-06-05 17:45:09 +0200

    update/delete Personne

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Entity\Personne;
 use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Authentication\RememberMe\PersistentToken;
 
 #[Route('personne')]
 class PersonneController extends AbstractController
@@ -54,7 +56,7 @@ class PersonneController extends AbstractController
     }
 
     // pagination à l'aide de findBy()
-    #[Route('/all/{page<\d+>?2}/{nbElem<\d+>?12}', name: 'app_personne.all')]
+    #[Route('/all/{page<\d+>?1}/{nbElem<\d+>?12}', name: 'app_personne.all')]
     public function getAll(PersistenceManagerRegistry $doctrine, $page, $nbElem): Response
     {
         $repository = $doctrine->getRepository(Personne::class);
@@ -90,5 +92,47 @@ class PersonneController extends AbstractController
         return $this->render('personne/detail.html.twig', [
             'personne' => $personne,
         ]);
+    }
+
+    // utilisation du Param Convertor
+    // se référer route:'app_personne.detail' pour une procédure sans Param Conv
+    #[Route('/delete/{id<\d+>}', name:'app_personne.delete')]
+    public function delete(Personne $personne = null, PersistenceManagerRegistry $doctrine): RedirectResponse {
+        if($personne){
+            $id_perso = strval($personne->getId());
+            $manager = $doctrine->getManager();
+            // ajoute l'action de suppression à la transaction
+            $manager->remove($personne);
+            // execute la transaction
+            $manager->flush();
+            $this->addFlash('success', "La personne (id:".$id_perso.") a été supprimée avec succés.");
+        } else {
+            $this->addFlash('error','La personne que vous essayez de supprimer n\'existe pas.' );
+        }
+        return $this->redirectToRoute('app_personne.all');
+    }
+
+    #[Route('/update/{id<\d+>}/{firstname?X}/{name?D}/{age?0}', name:'app_personne.update')]
+    public function update(Personne $personne = null, PersistenceManagerRegistry $doctrine, $firstname, $name, $age): RedirectResponse {
+        if($personne){
+            $personne->setFirstname($firstname);
+            $personne->setName($name);
+            $personne->setAge($age);
+
+            $manager = $doctrine->getManager();
+            $manager->persist($personne);
+            $manager->flush();
+
+            $this->addFlash(
+               'success',
+               'Personne modifiée avec succés.'
+            );
+        } else {
+            $this->addFlash(
+               'error',
+               'Personne inexistante.'
+            );
+        }
+        return $this->redirectToRoute('app_personne.all');
     }
 }
